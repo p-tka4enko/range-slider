c98a98db6c1e36751a9ed236a2c9f240182f4377
pasha-zolotukhin <pasha.zolotukhin98@gmail.com>
2019-04-22 17:53:35 +0700

    Создать класс ConfigNormalizer #5

--- src/js/model.js
+++ src/js/model.js
@@ -1,43 +1,18 @@
+import ConfigNormalizer from './config-normalizer';
+
 class Model {
   constructor(...configs) {
     this.handle = { FROM: 1, TO: 2 };
     this._currentHandle = null;
     this._observers = [];
-    this._initialConfig = {};
-    this._config = {};
-    this._error = {
-      OBSERVER_SHOULD_BE_FUNCTION: 'Тип наблюдателя должен быть function',
-      HANDLE_SHOULD_BE_NUMBER: 'Тип ползунка должен быть number',
-      HANDLE_CAN_BE_FROM_OR_TO: 'Ползунок может принимать значения только FROM или TO',
-      CONFIG_SHOULD_BE_OBJECT: 'Конфигурация должна быть object',
-      MIN_SHOULD_BE_NUMBER: 'Тип свойства min должно быть number',
-      MAX_SHOULD_BE_NUMBER: 'Тип свойства max должно быть number',
-      FROM_SHOULD_BE_NUMBER: 'Тип свойства from должно быть number',
-      TO_SHOULD_BE_NUMBER: 'Тип свойства to должно быть number',
-      STEP_SHOULD_BE_NUMBER: 'Тип свойства step должно быть number',
-      RANGE_SHOULD_BE_BOOLEAN: 'Тип свойства range должно быть boolean',
-      VERTICAL_SHOULD_BE_BOOLEAN: 'Тип свойства vertical должно быть boolean',
-      DISPLAY_HINT_SHOULD_BE_BOOLEAN: 'Тип свойства displayHint должно быть boolean',
-      DISPLAY_TRACKER_SHOULD_BE_BOOLEAN: 'Тип свойства displayTracker должно быть boolean',
-      DISPLAY_GRID_SHOULD_BE_BOOLEAN: 'Тип свойства displayGrid должно быть boolean',
-      GRID_STEP_SHOULD_BE_NUMBER: 'Тип свойства gridStep должно быть number',
-      DISABLED_SHOULD_BE_BOOLEAN: 'Тип свойства disabled должно быть boolean',
-      ON_INIT_SHOULD_BE_FUNCTION_OR_NULL: 'Тип свойства onInit должно быть function или null',
-      ON_START_SHOULD_BE_FUNCTION_OR_NULL: 'Тип свойства onStart должно быть function или null',
-      ON_SLIDE_SHOULD_BE_FUNCTION_OR_NULL: 'Тип свойства onSlide должно быть function или null',
-      ON_CHANGE_SHOULD_BE_FUNCTION_OR_NULL: 'Тип свойства onChange должно быть function или null',
-      ON_FINISH_SHOULD_BE_FUNCTION_OR_NULL: 'Тип свойства onFinish должно быть function или null',
-      ON_UPDATE_SHOULD_BE_FUNCTION_OR_NULL: 'Тип свойства onUpdate должно быть function или null',
-    };
+    this._initialConfig = null;
+    this._config = null;
+    this._configNormalizer = new ConfigNormalizer();
 
     this._initConfig(configs);
   }
 
   addObserver(observer) {
-    if (typeof observer !== 'function') {
-      throw new Error(this._error.OBSERVER_SHOULD_BE_FUNCTION);
-    }
-
     const duplicate = this._observers.filter(o => o === observer);
     if (duplicate.length > 0) {
       return false;
@@ -53,14 +28,6 @@ class Model {
   }
 
   startSlide(handle) {
-    if (typeof handle !== 'number' || Number.isNaN(handle)) {
-      throw new Error(this._error.HANDLE_SHOULD_BE_NUMBER);
-    }
-
-    if (handle !== this.handle.FROM && handle !== this.handle.TO) {
-      throw new Error(this._error.HANDLE_CAN_BE_FROM_OR_TO);
-    }
-
     if (this._currentHandle || this._config.disabled) {
       return false;
     }
@@ -87,6 +54,7 @@ class Model {
       return;
     }
 
+    const configNormalizer = this._configNormalizer;
     const config = this._config;
     const {
       min,
@@ -100,9 +68,9 @@ class Model {
     } = config;
 
     if (this._currentHandle === this.handle.FROM) {
-      config.from = this._getAlignedValue(value, min, range ? to : max, step);
+      config.from = configNormalizer.getAlignedValue(value, min, range ? to : max, step);
     } else if (this._currentHandle === this.handle.TO) {
-      config.to = this._getAlignedValue(value, from, max, step);
+      config.to = configNormalizer.getAlignedValue(value, from, max, step);
     }
 
     if ((config.from !== from) || (config.to !== to)) {
@@ -126,14 +94,12 @@ class Model {
   }
 
   validate(config) {
-    if (typeof config !== 'object' || config === null) {
-      throw new Error(this._error.CONFIG_SHOULD_BE_OBJECT);
-    }
-
+    const configNormalizer = this._configNormalizer;
     const validatedConfig = { ...this._config, ...config };
-    this._checkConfigTypes(validatedConfig);
 
-    return this._getAlignedConfig(validatedConfig);
+    configNormalizer.checkConfigTypes(validatedConfig);
+
+    return configNormalizer.getAlignedConfig(validatedConfig);
   }
 
   getNearestHandle(value) {
@@ -193,132 +159,6 @@ class Model {
     if (typeof func === 'function') {
       func(this._config);
     }
-  }
-
-  _checkConfigTypes(config) {
-    if (typeof config.min !== 'number' || Number.isNaN(config.min)) {
-      throw new Error(this._error.MIN_SHOULD_BE_NUMBER);
-    }
-
-    if (typeof config.max !== 'number' || Number.isNaN(config.max)) {
-      throw new Error(this._error.MAX_SHOULD_BE_NUMBER);
-    }
-
-    if (typeof config.from !== 'number' || Number.isNaN(config.from)) {
-      throw new Error(this._error.FROM_SHOULD_BE_NUMBER);
-    }
-
-    if (typeof config.to !== 'number' || Number.isNaN(config.to)) {
-      throw new Error(this._error.TO_SHOULD_BE_NUMBER);
-    }
-
-    if (typeof config.step !== 'number' || Number.isNaN(config.step)) {
-      throw new Error(this._error.STEP_SHOULD_BE_NUMBER);
-    }
-
-    if (typeof config.range !== 'boolean') {
-      throw new Error(this._error.RANGE_SHOULD_BE_BOOLEAN);
-    }
-
-    if (typeof config.vertical !== 'boolean') {
-      throw new Error(this._error.VERTICAL_SHOULD_BE_BOOLEAN);
-    }
-
-    if (typeof config.displayHint !== 'boolean') {
-      throw new Error(this._error.DISPLAY_HINT_SHOULD_BE_BOOLEAN);
-    }
-
-    if (typeof config.displayTracker !== 'boolean') {
-      throw new Error(this._error.DISPLAY_TRACKER_SHOULD_BE_BOOLEAN);
-    }
-
-    if (typeof config.displayGrid !== 'boolean') {
-      throw new Error(this._error.DISPLAY_GRID_SHOULD_BE_BOOLEAN);
-    }
-
-    if (typeof config.gridStep !== 'number' || Number.isNaN(config.gridStep)) {
-      throw new Error(this._error.GRID_STEP_SHOULD_BE_NUMBER);
-    }
-
-    if (typeof config.disabled !== 'boolean') {
-      throw new Error(this._error.DISABLED_SHOULD_BE_BOOLEAN);
-    }
-
-    if (typeof config.onInit !== 'function' && config.onInit !== null) {
-      throw new Error(this._error.ON_INIT_SHOULD_BE_FUNCTION_OR_NULL);
-    }
-
-    if (typeof config.onStart !== 'function' && config.onStart !== null) {
-      throw new Error(this._error.ON_START_SHOULD_BE_FUNCTION_OR_NULL);
-    }
-
-    if (typeof config.onSlide !== 'function' && config.onSlide !== null) {
-      throw new Error(this._error.ON_SLIDE_SHOULD_BE_FUNCTION_OR_NULL);
-    }
-
-    if (typeof config.onChange !== 'function' && config.onChange !== null) {
-      throw new Error(this._error.ON_CHANGE_SHOULD_BE_FUNCTION_OR_NULL);
-    }
-
-    if (typeof config.onFinish !== 'function' && config.onFinish !== null) {
-      throw new Error(this._error.ON_FINISH_SHOULD_BE_FUNCTION_OR_NULL);
-    }
-
-    if (typeof config.onUpdate !== 'function' && config.onUpdate !== null) {
-      throw new Error(this._error.ON_UPDATE_SHOULD_BE_FUNCTION_OR_NULL);
-    }
-  }
-
-  _getAlignedConfig(config) {
-    let {
-      min,
-      max,
-      step,
-      from,
-      to,
-    } = config;
-
-    min = +min.toFixed(4);
-    max = +max.toFixed(4);
-    step = +step.toFixed(4);
-
-    if (max < min) {
-      max = min;
-    }
-
-    from = this._getAlignedValue(from, min, max, step);
-    to = this._getAlignedValue(to, from, max, step);
-
-    if (from > to) {
-      from = to;
-    }
-
-    return {
-      ...config,
-      min,
-      max,
-      step,
-      from,
-      to,
-    };
-  }
-
-  _getAlignedValue(value, min, max, step) {
-    let alignedValue = value;
-    const sliderSize = max - min;
-
-    if (alignedValue < min) {
-      alignedValue = min;
-    } else if (alignedValue <= (max - (sliderSize % step))) {
-      const point1 = +(alignedValue - ((alignedValue - min) % step)).toFixed(4);
-      const point2 = +((alignedValue + step) - ((alignedValue - min) % step)).toFixed(4);
-      const average = (point1 + point2) / 2;
-      alignedValue = alignedValue < average ? point1 : point2;
-    } else {
-      alignedValue = max;
-    }
-
-    return alignedValue;
   }
 }
 
